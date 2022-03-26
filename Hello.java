4c43385d9e1ff3e1a4dc6c75dc2134a4d95b5ee0
zhangshuhao <2905154639@qq.com>
2022-03-26 12:50:37 +0800

    hot-fix test

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ public class Hello{
 	public void main(String[] args){
 		System.oyt.println("Hello World!2 Hot-fix");
 		System.oyt.println("Hello World!");
-		System.oyt.println("Hello World!");
+		System.oyt.println("Hello World! hot -fix");
 		System.oyt.println("Hello World!");
 		System.oyt.println("Hello World!");
 		System.oyt.println("Hello World!");
