6a4a71c32fd1ec4f2ea9264d951666d2621b085a
zhangshuhao <2905154639@qq.com>
2022-03-26 12:18:51 +0800

    Second Commit

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello{
 	public void main(String[] args){
-		System.oyt.println("Hello World!");
+		System.oyt.println("Hello World!2");
 		System.oyt.println("Hello World!");
 		System.oyt.println("Hello World!");
 		System.oyt.println("Hello World!");
